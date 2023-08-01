134858bd8a7227292bf3ff77a63bd4ae1c4b14ff
Jay-Jay23 <skosanajojo@gmail.com>
2023-08-01 10:13:26 +0200

    add a variable to all input for dynamic ip from user

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for ip in `seq 1 254`; do
-ping -c 192.168.8.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+ping -c $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
